8f803c16484d86e3abea88f82cf25fb9051536a2
Yan-CarlosIF <yan312005@gmail.com>
2025-07-28 18:27:05 -0300

    docs: added [GET] /users route documentation

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -4,6 +4,9 @@ import "@/infra/container";
 
 import cors from "cors";
 import express from "express";
+import fs from "fs";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
 
 import { errorHandler } from "@/infra/errors/error-handler.ts";
 import connectWithDatabase from "@/infra/typeorm";
@@ -25,6 +28,10 @@ app.use(express.json());
 app.use(rateLimiter);
 
 app.get("/health", (request, response) => response.sendStatus(200));
+
+const swaggerFile = path.join(__dirname, "../../../swagger.json");
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFile, "utf8"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(router);
 
